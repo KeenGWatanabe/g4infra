7e39a670c9dee8721a22192bcbd47143dac067a5
DaisyD <hitori.no.hasu@gmail.com>
2025-05-26 02:36:02 +0800

    Add Terraform S3 backend and DynamoDB lock table for dev env

---
+++ backend.tf
@@ -0,0 +1,15 @@
+  terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.66.1"
+    }
+  }
+  
+  # backend "s3" {
+  #   # bucket         = "ce-grp-4-tfstate-backend-dev"
+  #   # key            = "g4infra/dev/terraform.tfstate"
+  #   # region         = "us-east-1"
+  #   # dynamodb_table = "ce-grp-4-terraform-state-locks-dev"
+  # }
+}
